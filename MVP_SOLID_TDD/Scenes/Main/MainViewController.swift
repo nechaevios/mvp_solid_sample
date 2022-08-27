79dce7e039fac16722dc2b02a1edd94b762ef95a
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-08-27 15:12:03 +0300

    MainViewController iter 1: label + didTapButtonAction + setGreeting added

--- MVP_SOLID_TDD/Scenes/Main/MainViewController.swift
+++ MVP_SOLID_TDD/Scenes/Main/MainViewController.swift
@@ -2,10 +2,23 @@ import UIKit
 
 final class MainViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    var presenter: MainViewPresenterProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    @IBAction func didTapButtonAction(_ sender: Any) {
+        presenter.showGreeting()
+    }
+}
 
-
+extension MainViewController: MainViewProtocol {
+    func setGreeting(greeting: String) {
+        greetingLabel.text = greeting
+    }
 }

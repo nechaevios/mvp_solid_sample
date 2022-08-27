61e8ad50c86a531ebefab7585f9207bef2769fcb
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-08-27 15:03:55 +0300

    input + output protocols imp, presenter iter 1 imp

--- MVP_SOLID_TDD/Scenes/Main/MainViewPresenter.swift
+++ MVP_SOLID_TDD/Scenes/Main/MainViewPresenter.swift
@@ -1,5 +1,28 @@
 import Foundation
 
-final class MainViewPresenter {
+/// Output protocol
+protocol MainViewProtocol: AnyObject {
+    func setGreeting(greeting: String)
+}
+
+/// Input protocol
+protocol MainViewPresenterProtocol: AnyObject {
+    init(view: MainViewProtocol, person: Person)
+    func showGreeting()
+}
+
+final class MainViewPresenter: MainViewPresenterProtocol {
+    let view: MainViewProtocol
+    let person: Person
     
+    init(view: MainViewProtocol, person: Person) {
+        self.view = view
+        self.person = person
+    }
+    
+    /// Abstract view method setGreeting called
+    func showGreeting() {
+        let greeting = "Hi" + person.firstName + " " + person.lastName
+        view.setGreeting(greeting: greeting)
+    }
 }

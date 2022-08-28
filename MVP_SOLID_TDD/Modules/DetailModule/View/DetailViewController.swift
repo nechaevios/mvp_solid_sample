db9dd98a03bf8ef174304bb07d7fd33c97528643
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-08-28 15:30:36 +0300

    detail view setup + detail vc linked

--- MVP_SOLID_TDD/Modules/DetailModule/View/DetailViewController.swift
+++ MVP_SOLID_TDD/Modules/DetailModule/View/DetailViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    //MARK: - @IBOutlet
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    var presenter: DetailViewPresenterProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        presenter.setComment()
     }
 
+}
+
+extension DetailViewController: DetailViewProtocol {
+    func setComment(comment: Comment?) {
+        commentLabel.text = comment?.body
+    }
 }

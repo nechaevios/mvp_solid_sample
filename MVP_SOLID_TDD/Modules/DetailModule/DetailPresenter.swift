6b71ce22aa2f3ff59f84e641647d8cdebc4e19e2
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-08-28 13:50:21 +0300

    DetailPresenter created

---
+++ MVP_SOLID_TDD/Modules/DetailModule/DetailPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  DetailPresenter.swift
+//  MVP_SOLID_TDD
+//
+//  Created by Nechaev Sergey  on 28.08.2022.
+//
+
+import Foundation
+
+protocol DetailPresenterProtocol {
+    
+}
+
+final class DetailPresenter {
+    
+}

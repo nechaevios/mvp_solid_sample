f17c3fd45cfccd6f0ebc903e2f201ef94a2a8569
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-08-29 12:58:53 +0300

    app launching  refactored, added router

--- MVP_SOLID/Application/SceneDelegate.swift
+++ MVP_SOLID/Application/SceneDelegate.swift
@@ -11,10 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         
-        let mainVC = ModuleBuilder.createMainModule()
-        let navBar = UINavigationController(rootViewController: mainVC)
+        let navigationController = UINavigationController()
+        let assemblyBuilder = ModuleBuilder()
+        let router = Router(navigationController: navigationController, assemblyBuilder: assemblyBuilder)
+        router.initialViewController()
         
-        window?.rootViewController = navBar
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
